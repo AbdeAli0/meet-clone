c397a8586c14dbf09088b2c2d446b6a4b395f689
murtazaTesseract <murtaza@tesseract.in>
2021-03-18 21:01:20 +0530

    meet clone change page

--- server.js
+++ server.js
@@ -15,15 +15,36 @@ app.use([
   Routes,
 ]);
 
-const io = (module.exports.io = require("socket.io")(server, {
-    cors: {
-      origin: "http://localhost:3000",
-      credentials: true
-    }
-  }));
+const io = (module.exports.io = require("socket.io")(server));
 const socketManager = require("./app/socketManager");
 io.on("connection", socketManager);
 
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+const path = require('path');
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+// ** MIDDLEWARE ** //
+const whitelist = ['http://localhost:3000'​, 'http://localhost:8080'​, 'https://shrouded-journey-38552.heroku...​]
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin)
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(cors(corsOptions))
